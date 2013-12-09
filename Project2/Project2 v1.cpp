c4f56420402bfaa5d5c0bc3bf44cc3d8dcc78d03
Dark66 <pcalderon2@student.rcc.edu>
2013-12-09 14:43:07 -0800

    Signed-off-by: Dark66 <pcalderon2@student.rcc.edu>

---
+++ Project2/Project2 v1.cpp
@@ -0,0 +1,72 @@
+
+#include <iostream>
+#include <cctype>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+///Global constants
+///Max string length
+const short STRING_SIZE=16;
+
+///New cost entries
+const short COL=2;
+
+///Name Length
+const short SIZE = 24;
+
+///Pay Structure
+struct Pay
+{
+
+};
+
+
+///Enter Name
+char *enterName (char* ,short l);
+///Displays Menu
+void menu();Synthesizer
+///Runs program
+void default_function();
+///Calculates Total pay
+float totalpay(float &refPay);
+///Calculates Net pay
+float net(float t,float e);
+///Enter Starting Expense values
+void startingValues(char array[][STRING_SIZE],float cost[][COL],short ne);
+
+///Starting Function [Main]
+int main()
+{
+
+}
+
+void menu()
+{
+
+}
+
+void default_function()
+{
+
+}
+char *enterName (char* array,short l)
+{
+
+}
+
+void startingValues(char array[][STRING_SIZE],float cost[][COL],short ne)
+{
+
+}
+
+float totalpay(float &ref)
+{
+
+}
+
+float net(float t, float e)///Declare Public Functions
+{
+
+}
